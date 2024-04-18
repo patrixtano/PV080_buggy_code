9bb1c3c898da5884f5a517fc1a4cd3255c92f272
Patrik Stano <patrikstano@eduroam45-94.fi.muni.cz>
2024-04-18 10:53:05 +0200

    fixed code injection

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -52,9 +52,6 @@ if __name__ == '__main__':
     if choice == "1": 
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
-    elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
-        fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
